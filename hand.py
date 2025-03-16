6060c7a520a60c8109d0dcb369e1e72965e83d2f
Edmund <edmundgachanja@gmail.com>
2025-03-16 13:52:21 +0300

    feat: adds fps meter

--- hand.py
+++ hand.py
@@ -10,6 +10,9 @@ hands = mp_hands.Hands()
 
 mp_draw = mp.solutions.drawing_utils
 
+prev_time = 0
+current_time = 0
+
 
 while True:
     success, frame = cap.read() # reads image
@@ -23,7 +26,13 @@ while True:
             mp_draw.draw_landmarks(frame, hand_lms,mp_hands.HAND_CONNECTIONS) # draws land marks
             
 
-    
+    #frame rate
+    current_time = time.time()
+    fps = 1/ (current_time - prev_time)
+    prev_time = current_time
 
-    cv2.imshow("image", frame) #displays the image
+    #display
+    cv2.putText(frame, str(int(fps)), (10, 70), cv2.FONT_HERSHEY_PLAIN, 3, (255, 0, 255), 2) 
+    cv2.imshow("image", frame) 
+    
     cv2.waitKey(1)
